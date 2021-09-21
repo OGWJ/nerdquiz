c6a09a538b620b04133e9db9871871d7d4c1cbca
OGWJ <owenglynwilliamjenkins@gmail.com>
2021-09-21 12:45:55 +0100

    added conditional rendering of rooms

--- client/src/pages/HomePage/index.js
+++ client/src/pages/HomePage/index.js
@@ -1,9 +1,21 @@
-import React from "react";
-import { useParams } from "react-router";
+import React, { useState } from "react";
+import { socket } from "../../service/socket";
 
 import './style.css'
 
 const HomePage = () => {
+
+  // Need to get all available rooms
+
+  const [rooms, setRooms] = useState([{
+    admin: 'jessica',
+    category: 'Board Games',
+    difficulty: 'medium'
+  }]);
+
+  socket.on('create room', newRoom => {
+    setRooms(prev => prev + newRoom);
+  })
 
   const getCardColors = difficulty => {
     // (OGWJ) TODO: should make difficulties and colors enums for robustness
@@ -17,6 +29,10 @@ const HomePage = () => {
     }
   }
 
+  const joinRoom = () => {
+    console.log('joined room')
+  }
+
   return (
     <div className='container mt-4 p-nav'>
       <h3 className='m-4'>Welcome {localStorage.getItem('username')}</h3>
@@ -25,30 +41,19 @@ const HomePage = () => {
       </div>
       <ul className='container'>
         <h3 className='mt-4'>Join a Room</h3>
-        <li className='card text-center my-3 p-3' style={{
-          backgroundColor: getCardColors('EASY').bg,
-          color: getCardColors('EASY').fg
-        }}>
-          <h3>Room A</h3>
-          <span>Category A</span>
-          <span>EASY</span>
-        </li>
-        <li className='card text-center my-3 p-3' style={{
-          backgroundColor: getCardColors('MEDIUM').bg,
-          color: getCardColors('MEDIUM').fg
-        }}>
-          <h3>Room B</h3>
-          <span>Category B</span>
-          <span>Medium</span>
-        </li>
-        <li className='card text-center my-3 p-3' style={{
-          backgroundColor: getCardColors('HARD').bg,
-          color: getCardColors('HARD').fg
-        }}>
-          <h3>Room C</h3>
-          <span>Category C</span>
-          <span>Hard</span>
-        </li>
+
+        {rooms.map(room => {
+          return (<li className='card text-center my-3 p-3' style={{
+            backgroundColor: getCardColors('EASY').bg,
+            color: getCardColors('EASY').fg
+          }}>
+            <h3>{room.admin}'s Room</h3>
+            <span>{room.category}</span>
+            <span>{room.difficulty}</span>
+            <button onClick={joinRoom(room.admin)}>Join</button>
+          </li>
+          )
+        })}
       </ul>
     </div>
   )
