08e8abf6e3e2a741852b9144d51f22babf03c2c8
OGWJ <owenglynwilliamjenkins@gmail.com>
2021-09-21 18:19:29 +0100

    added more styling and conditional rendering of table on quizfinished page

--- client/src/pages/QuizFinishedPage/index.js
+++ client/src/pages/QuizFinishedPage/index.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { GameContext, GameStateTypes } from "../../models/GameStateTypes";
 import Container from "react-bootstrap/Container";
 
@@ -11,13 +11,24 @@ const Loading = () => {
 const QuizFinishedPage = () => {
 
   const game = useContext(GameContext);
-  const [hasScores, setHasScores] = useState(false);
+  // const [scores, setScores] = useState(null);
+  const fakeScores = [
+    { position: 1, name: 'sophie', score: 15 },
+    { position: 2, name: 'fred', score: 10 },
+    { position: 3, name: 'phil', score: 4 },
+    { position: 4, name: 'henry', score: 0 }
+  ]
+  const [scores, setScores] = useState(fakeScores)
 
   const handleClick = () => {
     game.setState(GameStateTypes.HOME);
   }
 
-  if (!hasScores) {
+  useEffect(() => {
+    // get the scores, update state!
+  }, [])
+
+  if (!scores) {
     return (
       <div className='vh-100 d-flex align-items-center justify-content-center'>
         <Loading />
@@ -32,26 +43,26 @@ const QuizFinishedPage = () => {
               <tr className='d-flex justify-content-between'>
                 <th scope='col'>Rank</th>
                 <th scope='col'>Username</th>
-                <th scope='col'>Category</th>
                 <th scope='col'>Score</th>
               </tr>
             </thead>
             <tbody>
-              <tr className='d-flex justify-content-between'>
-                <th scope='row'>1</th>
-                <td>Mad Lad</td>
-                <td>Video Games</td>
-                <td>69420</td>
-              </tr>
-              <tr className='d-flex justify-content-between'>
-                <th scope='row'>2</th>
-                <td>Big Sal 6</td>
-                <td>Marvel Films</td>
-                <td>666</td>
-              </tr>
+              {
+                scores.map(user => {
+                  return (
+                    <>
+                      <tr className='d-flex justify-content-between'>
+                        <th scope='row'>{user.position}</th>
+                        <td>{user.name}</td>
+                        <td>{user.score}</td>
+                      </tr>
+                    </>
+                  )
+                })
+              }
             </tbody>
           </table>
-          <button onClick={handleClick}>Go Again?</button>
+          <button onClick={handleClick} className='btn btn-success'>Go Again?</button>
         </div>
       </>
     )
