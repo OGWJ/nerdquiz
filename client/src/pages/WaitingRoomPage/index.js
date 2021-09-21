122e7a15e053a79b7d17863201d08ed6b4b238f1
OGWJ <owenglynwilliamjenkins@gmail.com>
2021-09-21 13:11:58 +0100

    added conditional rendering of player cards in waitingroom

--- client/src/pages/WaitingRoomPage/index.js
+++ client/src/pages/WaitingRoomPage/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { socket } from "../../service/socket";
 import './style.css';
 
@@ -7,7 +7,7 @@ const WaitingRoomPage = () => {
   // todo get colors for each player
   // with const colorList
 
-  const [players, setPlayers] = useState([]);
+  const [players, setPlayers] = useState(['jessica', 'bill']);
 
   // Listen for others entering room to update the state
   socket.on('user enter room', eventInfo => {
@@ -19,18 +19,20 @@ const WaitingRoomPage = () => {
     setPlayers(prev => prev.filter(player => player != eventInfo.player));
   })
 
+  // TODO create color generation stuff
+
   return (
     <div className='p-nav'>
       <h3 className='pt-4 px-4'>Hostmame's Room</h3>
       <h5 className='px-4'><small>Category is</small> <em>Video Games</em></h5>
       <h5 className='px-4'><small>Difficulty is</small> <em>Hard!</em></h5>
       <ul>
-        <li className='card user-card text-center d-flex flex-column justify-content-center my-3'
-          style={{ backgroundColor: '#4e4d83', color: '#ffffff' }}>Username A</li>
-        <li className='card user-card text-center d-flex flex-column justify-content-center my-3'
-          style={{ backgroundColor: '#24a14b', color: '#ffffff' }}>Username B</li>
-        <li className='card user-card text-center d-flex flex-column justify-content-center my-3'
-          style={{ backgroundColor: '#ea323d', color: '#ffffff' }}>Username C</li>
+        {
+          players.map(player => {
+            return (<li className='card user-card text-center d-flex flex-column justify-content-center my-3'
+              style={{ backgroundColor: '#4e4d83', color: '#ffffff' }}>{player}</li>);
+          })
+        }
       </ul>
     </div >
   );
