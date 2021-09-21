6054e1ab8a2603f92c69d59712b1c9c2c478d21f
OGWJ <owenglynwilliamjenkins@gmail.com>
2021-09-21 16:58:29 +0100

    added clock simulation to quiz page for testing

--- client/src/pages/QuizPage/index.js
+++ client/src/pages/QuizPage/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import ProgressBar from 'react-bootstrap/ProgressBar';
 import Container from 'react-bootstrap/Container'
 import { userExitsRoomHandler } from "../../handlers/userRoomInteractionHandlers";
@@ -10,9 +10,40 @@ import './style.css'
 const QuizPage = () => {
 
   const game = useContext(GameContext);
-  const [isUserTurn, setIsUserTurn] = useState(false);
+  const [isUserTurn, setIsUserTurn] = useState(true);
   const [question, setQuestion] = useState('');
-  const [count, setCount] = useState(null);
+  const [options, setOptions] = useState(['A', 'B', 'C']);
+  // temporary hardcoding of count
+  const [count, setCount] = useState(10);
+
+  const startSimulateClock = () => {
+    let localCount = count;
+    setInterval(() => {
+      localCount--;
+      console.log(localCount);
+
+      if (localCount > 0) {
+        setCount(prev => prev - 1);
+      }
+
+      if (localCount < 1) {
+        setIsUserTurn(prev => !prev)
+        setCount(10);
+        localCount = 10;
+      }
+
+    }, 1000);
+  }
+
+  useEffect(() => {
+    startSimulateClock();
+  }, [])
+
+  const submitAnswer = () => {
+    // stub
+    console.log('submitted answer')
+    setIsUserTurn(prev => !prev);
+  }
 
   // add logic to listen for change of user turn
   socket.on('answer question', (quizInfo) => {
@@ -38,16 +69,22 @@ const QuizPage = () => {
       <button onClick={exitHandler}>Exit Quiz</button>
       <div className='container d-flex flex-column text-center'>
         <h3>Question is: {question}</h3>
-        <h4>secondsRemaining</h4>
+        <h4>{count} {count === 1 ? 'second' : 'seconds'} remaining...</h4>
         <Container>
-          <ProgressBar now={60} />
+          <ProgressBar animated now={count * 10} className='transition-one-second' />
         </Container>
         {isUserTurn ?
           <div>
-            {quizInfo.options.map(option => {
-              return <button onClick={submitAnswer(option)}>{option}</button>
+            {options.map(option => {
+              return <button onClick={() => submitAnswer(option)} className='btn btn-primary m-4'>{option}</button>
             })}
-          </div> : <h4>Please wait for your turn</h4>}
+          </div> : <div>
+            <h4>Please wait for your turn</h4>
+            {options.map(option => {
+              return <button disabled className='btn btn-disabled mx-4'>{option}</button>
+            })}
+          </div>
+        }
       </div>
     </div >
   );
