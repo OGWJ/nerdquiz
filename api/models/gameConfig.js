d4732061024494a70422af152e995d7a7b873c20
“ELOBO5” <elicialobo@hotmail.com>
2021-09-22 17:34:10 +0100

    update create method

--- api/models/gameConfig.js
+++ api/models/gameConfig.js
@@ -1,40 +1,41 @@
-
 class GameConfig {
-  constructor(roomId) {
+  constructor(roomId, category, difficulty, admin) {
     this.roomId = roomId;
-    // this.genre = data.genre;
-    // this.difficulty = data.difficulty;
-    // this.admin = data.admin;
+    this.category = category;
+    this.difficulty = difficulty;
+    this.admin = admin;
     this.users = [
       {
         name: this.roomId,
-        score: 0,
-        
+        score: 0
       }
     ];
     //this.startGame = false;
   }
   static gameData = [];
 
-  static create(id, q, a) {
-    const newGame = new GameConfig(id,{...q},{...a});
+  static create(admin, category, difficulty) {
+    const newGame = new GameConfig(admin, category, difficulty, admin);
     GameConfig.gameData.push(newGame);
     return newGame;
-}
-
-  static joinUser(roomId, username){
-    let currentGame = GameConfig.gameData.find(game => game.roomId === roomId)
-    if(currentGame){ currentGame.users.push({user: username, score: 0}) }
   }
 
-  static getAllUsers(roomId){
-    let currentGame = GameConfig.gameData.find(game => game.roomId === roomId)
-    return currentGame.users
+  static joinUser(roomId, username) {
+    let currentGame = GameConfig.gameData.find(
+      (game) => game.roomId === roomId
+    );
+    if (currentGame) {
+      currentGame.users.push({ user: username, score: 0 });
+    }
   }
 
+  static getAllUsers(roomId) {
+    let currentGame = GameConfig.gameData.find(
+      (game) => game.roomId === roomId
+    );
+    console.log(currentGame);
+    return currentGame.users;
+  }
 }
-
-
-
 
 module.exports = { GameConfig };
