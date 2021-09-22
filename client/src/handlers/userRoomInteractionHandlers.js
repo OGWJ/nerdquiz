0b3716ab350efe6be43a6f6480576a2bbd552713
“ELOBO5” <elicialobo@hotmail.com>
2021-09-22 17:36:10 +0100

    changed handlers a bit

--- client/src/handlers/userRoomInteractionHandlers.js
+++ client/src/handlers/userRoomInteractionHandlers.js
@@ -1,14 +1,18 @@
-import { socket } from "../service/socket"
+import { socket } from "../service/socket";
 
 export const userEntersRoomHandler = (roomId) => {
-    socket.emit('user enter room', { 'username': localStorage.getItem('username'), 'roomId': roomId });
-}
+  let roomSettings = {
+    username: localStorage.getItem("username"),
+    roomId: roomId
+  };
+  socket.emit("user enter room", roomSettings);
+};
 
 export const userExitsRoomHandler = () => {
-    socket.emit('user exit room', localStorage.getItem('username'));
-}
+  socket.emit("user exit room", localStorage.getItem("username"));
+};
 
 export const userStartsQuizHandler = (roomId) => {
-    console.log("userStartsQuizHandler is called")
-    socket.emit('user start quiz', roomId);
-}
+  console.log("userStartsQuizHandler is called");
+  socket.emit("user start quiz", roomId);
+};
--- client/src/pages/HomePage/index.js
+++ client/src/pages/HomePage/index.js
@@ -1,88 +1,108 @@
 import React, { useState, useContext } from "react";
 import { socket } from "../../service/socket";
-import { userEntersRoomHandler } from '../../handlers/userRoomInteractionHandlers';
+import { userEntersRoomHandler } from "../../handlers/userRoomInteractionHandlers";
 import { GameContext, GameStateTypes } from "../../models/GameStateTypes";
 
-import './style.css'
+import "./style.css";
 import { useEffect } from "react";
 
-
 const HomePage = () => {
-
   const game = useContext(GameContext);
 
   // Get all rooms on mount
   const [rooms, setRooms] = useState([]);
+
   useEffect(async () => {
-    let allRoomsOnMount = [{ admin: 'jessica', category: 'Board Games', difficulty: 'Hard' }] //await fetch(`${apiUrl}${allRoomsEndPoint}`)
+    socket.emit("get room list");
+    let allRoomsOnMount = [
+      { admin: "jessica", category: "Board Games", difficulty: "Hard" }
+    ]; //await fetch(`${apiUrl}${allRoomsEndPoint}`)
     setRooms(allRoomsOnMount);
-  }, [])
+  }, []);
 
   // Update rooms when they are created in real-time
-  socket.on('room created', newRoom => {
-    console.log(newRoom.admin)
-    setRooms(rooms.push({admin: newRoom.admin, category: newRoom.category, difficulty: newRoom.difficulty}));
-    console.log("new room")
-    console.log(rooms)
-  })
+  socket.on("room created", (newRoom) => {
+    console.log(newRoom.admin);
+    setRooms(
+      rooms.push({
+        admin: newRoom.admin,
+        category: newRoom.category,
+        difficulty: newRoom.difficulty
+      })
+    );
+    console.log("new room");
+    console.log(rooms);
+  });
 
-  const joinRoom = roomAdmin => {
-    console.log('joined room')
-    socket.emit('joined room', {
-      username: localStorage.getItem('username'),
+  const joinRoom = (roomAdmin) => {
+    console.log("joined room");
+    socket.emit("joined room", {
+      username: localStorage.getItem("username"),
       roomAdmin: roomAdmin
-    })
+    });
 
     // NOTE: Line below for temporary for development without socket
-    game.setState(GameStateTypes.WAITING_ROOM)
-  }
+    game.setState(GameStateTypes.WAITING_ROOM);
+  };
 
-  socket.on('user enter room', () => {
-    game.setState(GameStateTypes.WAITING_ROOM)
-  })
+  socket.on("room list", (allGames) => {
+    console.log(allGames);
+    setRooms(allGames);
+  });
+
+  socket.on("user enter room", () => {
+    game.setState(GameStateTypes.WAITING_ROOM);
+  });
 
   const handleCreateRoom = () => {
     // stub
-    game.setState(GameStateTypes.CREATE_ROOM)
-  }
+    game.setState(GameStateTypes.CREATE_ROOM);
+  };
 
-  const getCardColors = difficulty => {
+  const getCardColors = (difficulty) => {
     // (OGWJ) TODO: should make difficulties and colors enums for robustness
     switch (difficulty) {
-      case 'EASY':
-        return { bg: '#2fa14b', fg: '#ffffff' }
-      case 'MEDIUM':
-        return { bg: '#1daadd', fg: '#ffffff' }
-      case 'HARD':
-        return { bg: '#4e4d83', fg: '#ffffff' }
+      case "EASY":
+        return { bg: "#2fa14b", fg: "#ffffff" };
+      case "MEDIUM":
+        return { bg: "#1daadd", fg: "#ffffff" };
+      case "HARD":
+        return { bg: "#4e4d83", fg: "#ffffff" };
     }
-  }
-
+  };
 
   return (
-    <div className='container mt-4 p-nav'>
-      <h3 className='m-4'>Welcome {localStorage.getItem('username')}</h3>
-      <div className='container d-flex justify-content-center'>
-        <button className='btn btn-secondary' onClick={handleCreateRoom}>Create a Room</button>
+    <div className="container mt-4 p-nav">
+      <h3 className="m-4">Welcome {localStorage.getItem("username")}</h3>
+      <div className="container d-flex justify-content-center">
+        <button className="btn btn-secondary" onClick={handleCreateRoom}>
+          Create a Room
+        </button>
       </div>
-      <ul className='container'>
-        <h3 className='mt-4'>Join a Room</h3>
+      <ul className="container">
+        <h3 className="mt-4">Join a Room</h3>
 
-        {rooms.map(room => {
-          return (<li className='card text-center my-3 p-3' style={{
-            backgroundColor: getCardColors('EASY').bg,
-            color: getCardColors('EASY').fg
-          }}>
-            <h3>{room.admin}'s Room</h3>
-            <span>{room.category}</span>
-            <span>{room.difficulty}</span>
-            <button onClick={(e) => userEntersRoomHandler(room.admin)}>Join</button>
-          </li>
-          )
+        {rooms.map((room) => {
+          return (
+            <li
+              className="card text-center my-3 p-3"
+              style={{
+                backgroundColor: getCardColors("EASY").bg,
+                color: getCardColors("EASY").fg
+              }}
+            >
+              <h3>{room.admin}'s Room</h3>
+              <span>{room.category}</span>
+              <span>{room.difficulty}</span>
+              <button onClick={(e) => userEntersRoomHandler(room.admin)}>
+                Join
+              </button>
+            </li>
+          );
         })}
       </ul>
     </div>
-  )
+  );
 };
 
 export default HomePage;
