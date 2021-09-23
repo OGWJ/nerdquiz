2e5fed85f9a5d62eaa239aa8bbf13a64515114b9
“ELOBO5” <elicialobo@hotmail.com>
2021-09-23 13:08:59 +0100

    add method to find user by socket id

--- api/models/gameConfig.js
+++ api/models/gameConfig.js
@@ -1,11 +1,12 @@
 class GameConfig {
-  constructor(roomId, category, difficulty, admin) {
+  constructor(roomId, category, difficulty, admin, socketId) {
     this.roomId = roomId;
     this.category = category;
     this.difficulty = difficulty;
     this.admin = admin;
     this.users = [
       {
+        socketId: this.socketId,
         user: this.roomId,
         score: 0
       }
@@ -14,18 +15,24 @@ class GameConfig {
   }
   static gameData = [];
 
-  static create(admin, category, difficulty) {
-    const newGame = new GameConfig(admin, category, difficulty, admin);
+  static create(admin, category, difficulty, socketId) {
+    const newGame = new GameConfig(
+      admin,
+      category,
+      difficulty,
+      admin,
+      socketId
+    );
     GameConfig.gameData.push(newGame);
     return newGame;
   }
 
-  static joinUser(roomId, username) {
+  static joinUser(roomId, username, socketId) {
     let currentGame = GameConfig.gameData.find(
       (game) => game.roomId === roomId
     );
     if (currentGame) {
-      currentGame.users.push({ user: username, score: 0 });
+      currentGame.users.push({ user: username, score: 0, socketId: socketId });
     }
   }
 
@@ -58,6 +65,12 @@ class GameConfig {
       (game) => game.roomId === roomId
     );
     return currentGame;
+  }
+
+  findUsernameBySocketId(socketId) {
+    let findUser = this.users.find((user) => user.socketId === socketId);
+    if (findUser) return findUser.username;
+    return;
   }
 }
 
